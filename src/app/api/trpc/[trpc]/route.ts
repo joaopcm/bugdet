fe6da3ca32136de54e691a5d6723d48dc5f2a2ae
João Melo <jopcmelo@gmail.com>
2025-04-15 17:31:27 -0300

    feat: add trpc

---
+++ src/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,12 @@
+import { appRouter } from '@/server'
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
+
+const handler = (request: Request) =>
+  fetchRequestHandler({
+    endpoint: '/api/trpc',
+    req: request,
+    router: appRouter,
+    createContext: () => ({}),
+  })
+
+export { handler as GET, handler as POST }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+'use client'
+
+import { trpc } from '@/lib/trpc/client'
+
 export default function Home() {
+  const greeting = trpc.greeting.useQuery()
+
   return (
     <main>
-      <div>Hello world!</div>
+      <div>{JSON.stringify(greeting.data)}</div>
     </main>
-  );
+  )
 }
