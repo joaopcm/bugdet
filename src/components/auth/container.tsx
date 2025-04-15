d3a768c9eeacbbbb7786332d6ec93e0144b6b5df
João Melo <jopcmelo@gmail.com>
2025-04-15 19:00:15 -0300

    feat(ui): add radix ui components and update theme styling
    - add all radix ui component packages
    - add styling for texture background and custom shadows
    - update button, input, and card components with new styling
    - add nunito and pt sans fonts
    - implement auth container component
    - update login form layout and styling

---
+++ src/components/auth/container.tsx
@@ -0,0 +1,44 @@
+import { env } from '@/env'
+import Image from 'next/image'
+import type { PropsWithChildren } from 'react'
+import { Card, CardContent } from '../ui/card'
+
+interface AuthContainerProps extends PropsWithChildren {
+  imageSource?: string
+}
+
+export function AuthContainer({
+  children,
+  imageSource = '/free-user.png',
+}: AuthContainerProps) {
+  return (
+    <div className="flex flex-col gap-6">
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          {children}
+
+          <div className="bg-muted relative hidden md:block">
+            <Image
+              priority
+              src={imageSource}
+              alt="Side illustration"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+              width={500}
+              height={500}
+            />
+          </div>
+        </CardContent>
+      </Card>
+
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our{' '}
+        <a href={`${env.NEXT_PUBLIC_APP_URL}/terms-of-service`}>
+          Terms of Service
+        </a>{' '}
+        and{' '}
+        <a href={`${env.NEXT_PUBLIC_APP_URL}/privacy-policy`}>Privacy Policy</a>
+        .
+      </div>
+    </div>
+  )
+}
