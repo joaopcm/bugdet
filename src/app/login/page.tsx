d3a70d4eaea38205324b79211219ffe3a851ed66
João Melo <jopcmelo@gmail.com>
2025-04-23 15:48:02 -0300

    refactor login page to use sign in form component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,10 @@
-import { LoginForm } from '@/components/auth/sign-in-form'
+import { SignInForm } from '@/components/auth/sign-in-form'
 
 export default function LoginPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <LoginForm />
+        <SignInForm />
       </div>
     </div>
   )
