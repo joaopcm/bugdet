7d7b9c9c2d34f45a90a949493783f57fce9b9d93
João Melo <jopcmelo@gmail.com>
2025-05-14 22:02:00 -0300

    add custom headers to disallow indexing on vercel preview environments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async headers() {
+    const headers = []
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      })
+    }
+    return headers
+  },
+}
 
-export default nextConfig;
+export default nextConfig
