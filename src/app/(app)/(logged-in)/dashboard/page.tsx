bf25fb2c5b6f467c9b667c68b49303fea06cb117
João Melo <jopcmelo@gmail.com>
2025-08-24 14:41:50 -0300

    refactor dashboard page to use page layout component with header and description

--- src/app/(app)/(logged-in)/dashboard/page.tsx
+++ src/app/(app)/(logged-in)/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import { PageLayout } from '@/components/logged-in/page-layout'
+
 export default function DashboardPage() {
-  return <div>Dashboard page</div>
+  return (
+    <PageLayout.Root>
+      <PageLayout.Header>
+        <PageLayout.HeaderContent>
+          <PageLayout.HeaderTitle title="Dashboard" />
+          <PageLayout.HeaderDescription description="View your financial life." />
+        </PageLayout.HeaderContent>
+      </PageLayout.Header>
+    </PageLayout.Root>
+  )
 }
