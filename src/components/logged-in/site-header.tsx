db56926bfba19be9fe75c953cc74cdceab3f1747
João Melo <jopcmelo@gmail.com>
2025-08-24 14:10:13 -0300

    feat(ui): add hover cursor pointer and sidebar shortcut tooltip for improved ux
    -  add hover cursor pointer to sidebar and dropdown menu buttons
    -  add tooltip with keyboard shortcut to sidebar trigger in site header

--- src/components/logged-in/site-header.tsx
+++ src/components/logged-in/site-header.tsx
@@ -1,10 +1,22 @@
+'use client'
+
 import { SidebarTrigger } from '@/components/ui/sidebar'
+import { Kbd, SHORTCUTS_VALUES } from '../ui/kbd'
+import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip'
 
 export function SiteHeader() {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <SidebarTrigger className="-ml-1 lg:-ml-4" />
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <SidebarTrigger className="-ml-1 lg:-ml-4" />
+          </TooltipTrigger>
+          <TooltipContent>
+            Or press <Kbd variant="outline">{SHORTCUTS_VALUES.CMD} + B</Kbd> to
+            toggle the sidebar
+          </TooltipContent>
+        </Tooltip>
       </div>
     </header>
   )
